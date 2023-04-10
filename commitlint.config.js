bb9590f61e3f78fe1d17fe42c0e0dcbb38463660
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-04-10 20:53:30 +0200

    chore(ci): setup workflow for release process

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["build", "chore", "docs", "feat", "fix", "refactor", "style", "test"]],
+    "body-max-line-length": [0, "always"],
+    "footer-max-line-length": [0, "always"],
+  },
+};
