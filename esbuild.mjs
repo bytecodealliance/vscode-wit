5127b80257d3f4317cd0842c9b59fbb8e8d67d82
Victor Adossi <vadossi@cosmonic.com>
2025-05-29 23:02:40 +0900

    chore: add prettier
    This commit adds prettier config and integrates directly with eslint
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>
    
    chore: fmt
    
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>
    
    chore: update configuration files and improve CI workflow
    
    Signed-off-by: Gordon Smith <GordonJSmith@gmail.com>

--- esbuild.mjs
+++ esbuild.mjs
@@ -29,9 +29,7 @@ const esbuildProblemMatcherPlugin = {
 
 async function main() {
     const ctx = await esbuild.context({
-        entryPoints: [
-            "src/extension.ts"
-        ],
+        entryPoints: ["src/extension.ts"],
         bundle: true,
         format: "cjs",
         minify: production,
@@ -41,9 +39,7 @@ async function main() {
         outfile: "dist/extension.js",
         external: ["vscode"],
         logLevel: "silent",
-        plugins: [
-            esbuildProblemMatcherPlugin,
-        ],
+        plugins: [esbuildProblemMatcherPlugin],
     });
     if (watch) {
         await ctx.watch();
@@ -53,7 +49,7 @@ async function main() {
     }
 }
 
-main().catch(e => {
+main().catch((e) => {
     console.error(e);
     process.exit(1);
 });
---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/**
+ * Prettier configuration
+ *
+ * This is set up mostly to match the output of tsc
+ *
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default {
+    trailingComma: "es5",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    printWidth: 120,
+};
